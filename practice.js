313c23649653fbe7477ed48290ca241169e5978f
Uthman <osquare98@gmail.com>
2025-01-17 21:52:06 +0100

    Version 1

---
+++ practice.js
@@ -0,0 +1,11 @@
+function practice(input) {
+  let output = [];
+  input.map(item => {
+    if(item >= 2 && item % 2 === 0){
+      return output.push(item)
+    }
+  });
+  alert(output);
+};
+
+practice([2,3,4,5,6,7,8,10]);
